bc3444e00338db3c89ced24c31970f56704adfc3
anuj <anujnijhawan@gmail.com>
2017-10-06 07:20:50 -0700

    Adding logging for redux actions, and updating DevTools syntax to the latest.

--- shared/redux/configureStore.js
+++ shared/redux/configureStore.js
@@ -4,26 +4,60 @@ import axios from 'axios';
 import reducer from '../reducers';
 
 function configureStore(initialState) {
-  const enhancers = compose(
-    // Middleware store enhancer.
-    applyMiddleware(
-      // Initialising redux-thunk with extra arguments will pass the below
-      // arguments to all the redux-thunk actions. Below we are passing a
-      // preconfigured axios instance which can be used to fetch data with.
-      // @see https://github.com/gaearon/redux-thunk
-      thunk.withExtraArgument({ axios }),
-    ),
-    // Redux Dev Tools store enhancer.
-    // @see https://github.com/zalmoxisus/redux-devtools-extension
-    // We only want this enhancer enabled for development and when in a browser
-    // with the extension installed.
-    process.env.NODE_ENV === 'development' &&
+  // Initialising redux-thunk with extra arguments will pass the below
+  // arguments to all the redux-thunk actions. Below we are passing a
+  // preconfigured axios instance which can be used to fetch data with.
+  // @see https://github.com/gaearon/redux-thunk
+
+  const middlewares = [thunk.withExtraArgument({ axios })];
+
+  // Enable redux-logger in development mode, with specially composed options.
+  // This shows redux action logs in the console
+  if (process.env.BUILD_FLAG_IS_DEV) {
+    let logger;
+
+    /* eslint-disable no-undef */
+    if (__SERVER__) {
+      /* eslint-disable-next-line global-require */
+      const createCLILogger = require('redux-cli-logger').default;
+      logger = createCLILogger({});
+    } else {
+      /* eslint-disable-next-line global-require */
+      const { createLogger } = require('redux-logger');
+
+      logger = createLogger({
+        logger: console,
+        duration: true,
+        diff: true,
+        collapsed: true,
+      });
+    }
+
+    middlewares.push(logger);
+  }
+
+  // Redux Dev Tools store enhancer.
+  // @see https://github.com/zalmoxisus/redux-devtools-extension
+  // We only want this enhancer enabled for development and when in a browser
+  // with the extension installed.
+
+  /* eslint-disable no-underscore-dangle */
+
+  const canDevTool =
+    process.env.BUILD_FLAG_IS_DEV &&
+    process.env.BUILD_FLAG_IS_CLIENT &&
     typeof window !== 'undefined' &&
-    typeof window.devToolsExtension !== 'undefined'
-      ? // Call the brower extension function to create the enhancer.
-        window.devToolsExtension()
-      : // Else we return a no-op function.
-        f => f,
+    typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+  const composeEnhancers = canDevTool
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ serialize: { options: true } })
+    : compose;
+
+  /* eslint-enable */
+
+  const enhancers = composeEnhancers(
+    // Middleware store enhancer.
+    applyMiddleware(...middlewares),
   );
 
   const store = initialState
@@ -43,7 +77,7 @@ function configureStore(initialState) {
   return store;
 }
 
-// NOTE: If we create an '/api' endpoint in our application then we will neeed to
+// NOTE: If we create an '/api' endpoint in our application then we will need to
 // configure the axios instances so that they will resolve to the proper URL
 // endpoints on the server. We have to provide absolute URLs for any of our
 // server bundles. To do so we can set the default 'baseURL' for axios. Any
