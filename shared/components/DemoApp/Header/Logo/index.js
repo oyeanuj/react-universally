45bfa04a9b143af0a39298bb2214773956bb3e96
anuj <anujnijhawan@gmail.com>
2017-10-07 11:11:07 -0700

    Comment out images and markdown to zero in on the problem

--- shared/components/DemoApp/Header/Logo/index.js
+++ shared/components/DemoApp/Header/Logo/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.png';
+// import logo from './logo.png';
 
 function Logo() {
-  return <img src={logo} alt="Logo" style={{ width: '100px' }} />;
+  // return <img src={logo} alt="Logo" style={{ width: '100px' }} />;
+  return <h1>This is the logo!</h1>;
 }
 
 export default Logo;
